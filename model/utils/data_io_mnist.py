ab9d919d88762a22f44081f00f88d51d3a5746f5
Erik Nijkamp <erik.nijkamp@gmail.com>
2018-03-17 23:16:17 -0700

    ...

--- model/utils/data_io_mnist.py
+++ model/utils/data_io_mnist.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 import numpy as np
@@ -10,12 +11,15 @@ class DataSetMnist(object):
     def __init__(self, data_path, image_size=64, num_images=10000):
         data = input_data.read_data_sets(data_path, one_hot=True)
         self.image_size = image_size
-        self.images = np.zeros((num_images, image_size, image_size, 3)).astype(float)
+        self.images = np.zeros((num_images, image_size, image_size, 1)).astype(float)
         print('Loading dataset: {}'.format(data_path))
+
+        batch = data.train.next_batch(num_images)[0]
+        batch_tensor = tf.reshape(batch, [10, 28, 28, 1])
+        images = tf.image.resize_images(batch_tensor, [image_size, image_size]).eval()
+
         for i in range(num_images):
-            image = data.train.next_batch(1)[0]
-            image = image.resize((self.image_size, self.image_size))
-            image = np.array(image).astype(float)
+            image = np.array(images[i]).astype(float)
             max_val = image.max()
             min_val = image.min()
             image = (image - min_val) / (max_val - min_val) * 2 - 1
@@ -26,7 +30,7 @@ class DataSetMnist(object):
         return self.images
 
     def mean(self):
-        return np.mean(self.images, axis=(0, 1, 2, 3))
+        return np.mean(self.images, axis=(0))
 
     def to_range(self, low_bound, up_bound):
         min_val = self.images.min()
